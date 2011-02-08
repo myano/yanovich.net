55320b578e92be23d32eac6b6a7bcb45c68554e0
Michael S. Yanovich <yanovich.1@osu.edu>
2011-02-08 16:52:29 -0500

    added a todo page; still need to add link to main page.
    I need to find a second "sub" navigation system.

---
+++ todo-content.php
@@ -0,0 +1,6 @@
+This is an incomplete to-do list of "fun" projects, scripts, and anything else in between that I want to work on when I have free-time. There is no particular order.
+<br><br>
+<ul>
+    <li>Contribute, and help finish <a href="https://github.com/myano/sophia">Sophia</a></li>
+    <li>Install a blogging platform (looking at Blogofile) for this site.</li>
+</ul>
---
+++ todo.php
@@ -0,0 +1,5 @@
+<?php
+$page_title = "Michael Yanovich :: To-Do List";
+$content = "todo-content.php";
+include("template.php");
+?> 
