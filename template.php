6ac12a40e3ff4832a7a3230b4917fe4f01703bd5
Michael Yanovich <michael@yanovich.net>
2011-09-06 09:51:03 -0400

    moved javascript around to increase page performance (ever so slightly)

--- template.php
+++ template.php
@@ -7,18 +7,6 @@
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
         <meta name="keywords" content=" <?php echo $meta; ?> ">
         <title> <?php echo $page_title; ?> </title>
-        <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-21129437-1']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
     </head>
 
     <body>
@@ -67,5 +55,17 @@
         ?>
         </div>
 
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-21129437-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </body>
 </html>
