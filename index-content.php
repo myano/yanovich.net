8706f956ec8ab2e840ea19b06ff9d486123848a1
Michael Yanovich <michael@yanovich.net>
2011-11-06 17:07:41 -0500

    updated index to reflect the addition of SSL on my site

--- index-content.php
+++ index-content.php
@@ -1,12 +1,12 @@
 You have arrived at my personal website. If you need to contact me, please refer to the contact link above. 
 <br /><br />
-I'm currently transferring my domains, yanovich.net and yanovich.org to a new registrar, I apologize for any downtime over the next 24 hours through those two domains. You can still reach my site while those two domains are down via yano.me.
+SSL is now available on my site, click <a href="https://yanovich.net/">here</a> to use SSL. Please note that the certificate is only for yanovich.net.
 <br /><br />
 <a href="https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module"><span class="fontbold">How to Create a Phenny Module</span></a>
 <br><br>
 <span class="fontbold">Club Images</span><br>
 Ohio State Open Source pictures/images for the club are located <a href="http://opensource.osu.edu/~yanovich/open_source_images.tar.gz">here</a>.
 <br /><br />
-<span class="fontbold">OSUwireless</span><br>
-Here is the certificate you might need to connect to <span class="fontbold">OSUwireless</span> with <a href="Equifax_Secure_Certificate_Authority.cer">Equifax Secure Certificate Authority</a>
+<span class="fontbold">osuwireless</span><br>
+Here is the certificate you might need to connect to <span class="fontbold">osuwireless</span> with <a href="Equifax_Secure_Certificate_Authority.cer">Equifax Secure Certificate Authority</a>
 <br><br>
