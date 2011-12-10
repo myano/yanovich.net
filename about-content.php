dab5dc4d94d49db55ff910a134abb3a4824ef99e
Michael Yanovich <michael@yanovich.net>
2011-12-10 00:17:32 -0500

    minors have been put on hold

--- about-content.php
+++ about-content.php
@@ -1,2 +1,2 @@
 My name is Michael Yanovich.<br><br>
-I am a student at <a href="http://osu.edu/">The Ohio State University</a>. I'm pursuing a bachelors in <a href="http://www.cse.ohio-state.edu/">Computer Science and Engineering</a> and I'm minoring in Linguistics and Mathematics.
+I am a student at <a href="http://osu.edu/">The Ohio State University</a>. I'm pursuing a bachelors in <a href="http://www.cse.ohio-state.edu/">Computer Science and Engineering</a>.
