c0ae89a1533856ae4542166e4339e27886ce2a87
Michael Yanovich <michael@yanovich.net>
2011-12-10 00:19:36 -0500

    removed jenni instruction file; instructions for creating a phenny/jenni module can be found at https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module

--- create.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $page_title = "Michael Yanovich Open Source Page :: How to Create a Phenny Module";
-// $content = "create-content.php";
-// $meta = "phenny_osu, phenny, phenny module, create phenny module, module, python, how to create a phenny module, ohio state, open source";
-// include("template.php");
-header( 'Location: https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module' ) ;
-?> 
