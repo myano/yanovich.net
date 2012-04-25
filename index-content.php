0cff2eeabbe6a874ba57f2238e88ca0510aa3f78
Michael Yanovich <michael@yanovich.net>
2012-04-25 16:40:56 -0400

    brought in the images file locally

--- index-content.php
+++ index-content.php
@@ -5,7 +5,7 @@ SSL is now available on my site, click <a href="https://yanovich.net/">here</a> 
 <a href="https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module"><span class="fontbold">How to Create a Phenny Module</span></a>
 <br><br>
 <span class="fontbold">Club Images</span><br>
-Ohio State Open Source pictures/images for the club are located <a href="http://opensource.osu.edu/~yanovich/open_source_images.tar.gz">here</a>.
+Ohio State Open Source pictures/images for the club are located <a href="open_source_images.tar.gz">here</a>.
 <br /><br />
 <span class="fontbold">osuwireless</span><br>
 Here is the certificate you might need to connect to <span class="fontbold">osuwireless</span> with <a href="Equifax_Secure_Certificate_Authority.cer">Equifax Secure Certificate Authority</a>
