fd1d791cb1a4f2b1f40f357e4bb50b362a890a23
Michael Yanovich <michael@yanovich.net>
2012-04-27 13:41:16 -0400

    removed comment about site having SSL

--- index-content.php
+++ index-content.php
@@ -1,8 +1,6 @@
 You have arrived at my personal website. If you need to contact me, please refer to the contact link above. 
 <br /><br />
-SSL is now available on my site, click <a href="https://yanovich.net/">here</a> to use SSL. Please note that the certificate is only for yanovich.net.
-<br /><br />
-<a href="https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module"><span class="fontbold">How to Create a Phenny Module</span></a>
+<a href="https://github.com/myano/jenni/wiki/How-to-create-a-phenny-module"><span class="fontbold">How to Create a Phenny/Jenni Module</span></a>
 <br><br>
 <span class="fontbold">Club Images</span><br>
 Ohio State Open Source pictures/images for the club are located <a href="open_source_images.tar.gz">here</a>.
