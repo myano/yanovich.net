ac11c51119f7a84d68d0c2966dbba39c79d1a471
Michael Yanovich <michael@yanovich.net>
2013-02-15 19:36:03 +0000

    updated robots.txt to test and experiment of the /.*

--- footer.php
+++ footer.php
@@ -5,5 +5,5 @@
                 <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/">
                 <img class="noborder" alt="Creative Commons License" src="images/88x31.png" /></a>
                 <br />
-                <span class="hidden"><a href="https://bit.ly/1RwV" class="hidden" rel="contact" style="color:#000000;">047e8e33354ea9cd6deef16bccc5a1764a2d52dda92019f2285c40a2b5559f56e8a3803c6777ed818700d2b4fb4157c38753ea32f796e5eb5ee87bf14aed916c</a></span>
+                <span class="hidden"><a href="https://j.mp/VjbEVk" class="hidden" rel="contact" style="color:#000000;">047e8e33354ea9cd6deef16bccc5a1764a2d52dda92019f2285c40a2b5559f56e8a3803c6777ed818700d2b4fb4157c38753ea32f796e5eb5ee87bf14aed916c</a></span>
             </div>
