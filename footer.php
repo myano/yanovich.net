c88da772984c75682f04b94b43127e8a62e7e40c
Michael Yanovich <michael@yanovich.net>
2013-05-23 06:52:32 +0000

    updated footer

--- footer.php
+++ footer.php
@@ -2,8 +2,11 @@
                 <iframe style="border: 0; margin: 0; padding: 0;" src="https://www.gittip.com/myano/widget.html" width="48" height="20"></iframe>
                 <a href="https://flattr.com/thing/801771/Michael-Yanovichs" target="_blank"><img src="images/flattr-badge-large.png" alt="Flattr this" title="Flattr this" style="border: 0;" /></a>
                 <br />
-                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/">
-                <img class="noborder" alt="Creative Commons License" src="images/88x31.png" /></a>
+                <a href="https://coinbase.com/checkouts/beb93bfa56c3e638e19430e1f7392554" target="_blank"><span style="font-size:16px; color:#fff;">Donate Bitcoins</span></a>
                 <br />
+                <span style="font-size:14px;">LTC: LdfNEPFW7oxD5kevHgUY9yhTirZWPPne88</span>
+                <br />
+                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img class="noborder" alt="Creative Commons License" src="images/88x31.png" /></a>
+                <br /><br />
                 <span class="hidden"><a href="https://j.mp/VjbEVk" class="hidden" rel="contact" style="color:#000000;">047e8e33354ea9cd6deef16bccc5a1764a2d52dda92019f2285c40a2b5559f56e8a3803c6777ed818700d2b4fb4157c38753ea32f796e5eb5ee87bf14aed916c</a></span>
             </div>
