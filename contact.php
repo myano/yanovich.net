a6de631538a2654049b675f0570930bae42411f0
Michael Yanovich <michael@yanovich.net>
2013-08-16 01:58:16 +0000

    updated contact information

--- contact.php
+++ contact.php
@@ -3,12 +3,19 @@ $page_title = "Michael Yanovich :: Contact";
 $meta = "contact, yanovich";
 
 $content = <<<EOT
+<pre>
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+</pre>
 You can contact me using the following methods.
 <br /><br />
 <span class="fontbold"><em>Email:</em></span><br />
 <img src="images/mynet.jpg" /><br /><br />
 <img src="images/namenum.jpg" /><br /><br />
 
+You may also contact me via <a href="https://en.wikipedia.org/wiki/XMPP">XMPP</a> (jabber): msyano [at] dukgo (dot) com
+<br /><br />
 I use PGP when e-mailing. Please try to sign and encrypt your e-mails to me with my public key, which is located <a href="http://pgp.mit.edu:11371/pks/lookup?op=get&search=0xA06940BDC20EAE1D">here</a>. All e-mails sent by me will be signed. If you receive an e-mail from me that is not signed assumed it has been tampered with and was not sent by me (ie: the origin of the e-mail has been spoofed or altered).
 <br /><br />
 PGP Fingerprint:
@@ -25,6 +32,25 @@ SSL Certificate Fingerprints:
     <li><em>SHA-1: </em> 81 33 76 7C 4E 67 B6 05 6F 54 70 4D 70 FC 0E A4 78 8B 5A 8F</li>
     <li><em>MD5: </em> FD 2D D9 B0 2A 31 83 C4 33 07 91 D1 99 1C 51 84</li>
 </ul>
+<pre>
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.12 (GNU/Linux)
+
+iQIcBAEBAgAGBQJR0taSAAoJEKBpQL3CDq4d3joQAIRSNb6HnNYM38AipWqNh8x6
+9lLM1Pe3yCEolpPPbSGIsB92WqsTQTqPjSm7I7pO8raEoN4lEOIwdgStCWmHTDGu
+I2A1sASsCDgxBxWJr7liXVW7uhkiwN0avt52PdFb9k8H1caATwwCOEdJLQZvKsST
+pBS4I6Z93nluYICqBYg+olG8vOa14CsE/bX4yVwT35nbV/dSLAWZoUAlO4xdDvtu
+qpG0PPykft39OqkfOuKKWlcKwCksjQ6EgU9L0vextwbR9JwIKVkE8d2CY7LswDKT
+WiQ+oMyWR92MqE96KvyOOXaZ0/TMdLJjdtFJhL3qdMk37JmSl9aYLOIYh0ueUS2t
+HLVTFVBMSl95ap2diTtd9XswhhkugeHzBI0j4o/W9/auvB4rkOfNCG8jTrX+pW7L
+NgY5+dWp1RDbErnQo9ma/eSPx8o478GVHBvCjsP0/NBlgrFkJNrSOk6LARBviQoW
+dNMvKDWCEBhOEWjqtHUirMUMl/gq1SlHLIZ0Tvt+/GWdxvatjkaybV4KENf2M42b
+sTyos7pu9YVoHCTeC6eBP5YbF2IDUM/FjHJbq0LaIRgcgGNq+5e2WQGlhd/cVwY7
+5Cmz4K8sGcuqxIifW7Clc8augWtku3Zr9guT9PbpFO0I3ylyP2aDq6KBAH81h1rn
+eOnr04OTWeAM/j8T2n+P
+=SJmr
+-----END PGP SIGNATURE-----
+</pre>
 EOT;
 
 include("template.php");
