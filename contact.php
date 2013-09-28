877b5b631e29639ae898fdb698d5266fed6bf133
Michael Yanovich <michael@yanovich.net>
2013-09-28 00:19:55 +0000

    added wikipedia link

--- contact.php
+++ contact.php
@@ -5,18 +5,17 @@ $meta = "contact, yanovich";
 $content = <<<EOT
 <pre>
 -----BEGIN PGP SIGNED MESSAGE-----
-Hash: SHA1
+Hash: SHA512
 
 </pre>
 You can contact me using the following methods.
 <br /><br />
 <span class="fontbold"><em>Email:</em></span><br />
 <img src="images/mynet.jpg" /><br /><br />
-<img src="images/namenum.jpg" /><br /><br />
 
 You may also contact me via <a href="https://en.wikipedia.org/wiki/XMPP">XMPP</a> (jabber): msyano [at] dukgo (dot) com
 <br /><br />
-I use PGP when e-mailing. Please try to sign and encrypt your e-mails to me with my public key, which is located <a href="http://pgp.mit.edu:11371/pks/lookup?op=get&search=0xA06940BDC20EAE1D">here</a>. All e-mails sent by me will be signed. If you receive an e-mail from me that is not signed assumed it has been tampered with and was not sent by me (ie: the origin of the e-mail has been spoofed or altered).
+I use <a href="https://en.wikipedia.org/wiki/Pretty_Good_Privacy">PGP</a> when e-mailing. Please try to sign and encrypt your e-mails to me with my public key, which is located <a href="http://pgp.mit.edu:11371/pks/lookup?op=get&search=0xA06940BDC20EAE1D">here</a>. All e-mails sent by me will be signed. If you receive an e-mail from me that is not signed assumed it has been tampered with and was not sent by me (ie: the origin of the e-mail has been spoofed or altered).
 <br /><br />
 PGP Fingerprint:
 <ul>
@@ -36,19 +35,19 @@ SSL Certificate Fingerprints:
 -----BEGIN PGP SIGNATURE-----
 Version: GnuPG v1.4.12 (GNU/Linux)
 
-iQIcBAEBAgAGBQJR0taSAAoJEKBpQL3CDq4d3joQAIRSNb6HnNYM38AipWqNh8x6
-9lLM1Pe3yCEolpPPbSGIsB92WqsTQTqPjSm7I7pO8raEoN4lEOIwdgStCWmHTDGu
-I2A1sASsCDgxBxWJr7liXVW7uhkiwN0avt52PdFb9k8H1caATwwCOEdJLQZvKsST
-pBS4I6Z93nluYICqBYg+olG8vOa14CsE/bX4yVwT35nbV/dSLAWZoUAlO4xdDvtu
-qpG0PPykft39OqkfOuKKWlcKwCksjQ6EgU9L0vextwbR9JwIKVkE8d2CY7LswDKT
-WiQ+oMyWR92MqE96KvyOOXaZ0/TMdLJjdtFJhL3qdMk37JmSl9aYLOIYh0ueUS2t
-HLVTFVBMSl95ap2diTtd9XswhhkugeHzBI0j4o/W9/auvB4rkOfNCG8jTrX+pW7L
-NgY5+dWp1RDbErnQo9ma/eSPx8o478GVHBvCjsP0/NBlgrFkJNrSOk6LARBviQoW
-dNMvKDWCEBhOEWjqtHUirMUMl/gq1SlHLIZ0Tvt+/GWdxvatjkaybV4KENf2M42b
-sTyos7pu9YVoHCTeC6eBP5YbF2IDUM/FjHJbq0LaIRgcgGNq+5e2WQGlhd/cVwY7
-5Cmz4K8sGcuqxIifW7Clc8augWtku3Zr9guT9PbpFO0I3ylyP2aDq6KBAH81h1rn
-eOnr04OTWeAM/j8T2n+P
-=SJmr
+iQIcBAEBCgAGBQJSOEOvAAoJEKBpQL3CDq4d0xsQAJioNq0u+dP97P9IBnMCJqNh
+AMOf0fv/5RsYI6SiCwBGnFAlAFLVohzPPUO05ut0RCNtOEGYs3e8rKF2wScVQill
+WTs+te6UkllNWjJt2H64vea7ZKeajKNgbYAujZlZ+HWcLX+ngcZ7pSVtYJO0HdCL
+ealb7RY53MRQOh03EtJ0R5Oo3G1Tw2lfWI/LwuzXhagcWgkVNBBSdCDqafAyka19
+ghIecUmNVTww1JQj1TzGQhdRmOCmTsrIGPH9kHlq0zYp5nVuSHSBIIL9+eGLfeow
+qVxhMDMmr6X29TUiH8uwL94puBsTBt6LStsBVWVQ2DgFbFLZLRZBxLLDaZIsEQC4
+any1id2oSNQxjZcVc6ZcZTPazprbd43qBod39wd4yEcIMkYHvUtvNER85z76fhHR
+y6kECkKgpUO4WHy8bXm3Cl7Fe7N7obd+kyTflg8/uSZCttuld4/aewhbYcZ3hlzt
+M3B4xLzF9OG5cOiiuz02x6DAVtZWH5vhj17tnEwzeH3bxpg0oWorsmQaX9U18WTA
+1d1wd/6JTKb+MO2C3SHDjN8nx9fOREHecomVcXZ/dt9Qdh5xwl2lMM7VPNehffsy
+RgVaOts1Ux3X5UAa7rs8d1VA+ugezXvC0YO21q2d0gB4YS+5bcKnh1lXCLczoEd5
+fC7wTQKr4Ff4VOH+2vk8
+=UrrN
 -----END PGP SIGNATURE-----
 </pre>
 EOT;
