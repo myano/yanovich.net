e278611a7e56b59e2c5c0c2981e9daec982a4cb8
Michael Yanovich <michael@yanovich.net>
2013-11-08 07:38:59 +0000

    updated gittip plugin

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <div class="textalign-center">
-                <iframe style="border: 0; margin: 0; padding: 0;" src="https://www.gittip.com/myano/widget.html" width="48" height="20"></iframe>
-                <a href="https://flattr.com/thing/801771/Michael-Yanovichs" target="_blank"><img src="images/flattr-badge-large.png" alt="Flattr this" title="Flattr this" style="border: 0;" /></a>
+                <script data-gittip-username="myano" data-gittip-widget="button" src="//gttp.co/v1.js"></script>
+                <a href="https://flattr.com/thing/801771/Michael-Yanovichs" target="_blank"><img src="/images/flattr-badge-large.png" alt="Flattr this" title="Flattr this" style="border: 0;" /></a>
                 <br />
                 <a href="https://coinbase.com/checkouts/beb93bfa56c3e638e19430e1f7392554" target="_blank"><span style="font-size:16px; color:#fff;">Donate Bitcoins</span></a>
                 <br />
                 <span style="font-size:14px;">LTC: LdfNEPFW7oxD5kevHgUY9yhTirZWPPne88</span>
                 <br />
-                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img class="noborder" alt="Creative Commons License" src="images/88x31.png" /></a>
+                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img class="noborder" alt="Creative Commons License" src="/images/88x31.png" /></a>
                 <br /><br />
                 <span class="hidden"><a href="http://is.gd/6Clmu7" class="hidden" rel="contact" style="color:#000;">627ebad2081d6a6979fe22bffa9beeb5d94d18167fe390472051cb4f99b59dc0750ffb4b584dae276a5559c3407041286001721dfd005bd2dd8e57abfdb6651a</a></span>
             </div>
