070a25d7fd3d1835f6db31077e6ced8277ee46aa
Michael Yanovich <michael@yanovich.net>
2014-02-14 03:35:30 +0000

    updated creative commons license

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
                 <a href="https://coinbase.com/checkouts/3691d1c8e3c0ce435b9b022612cd6950"><span style="font-size:14px; color:#fff">Donate Bitcoins</span></a>
                 <br />
                 <span style="font-size:14px;">LTC: LdfNEPFW7oxD5kevHgUY9yhTirZWPPne88</span>
-                <br />
-                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img class="noborder" alt="Creative Commons License" src="/images/88x31.png" /></a>
+                <br /><br />
+                <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">yano</span> by <a xmlns:cc="https://creativecommons.org/ns#" href="https://yanovich.net/" property="cc:attributionName" rel="cc:attributionURL">yano</a> is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
                 <br /><br />
                 <span class="hidden"><a href="http://is.gd/6Clmu7" class="hidden" rel="contact" style="color:#000;">627ebad2081d6a6979fe22bffa9beeb5d94d18167fe390472051cb4f99b59dc0750ffb4b584dae276a5559c3407041286001721dfd005bd2dd8e57abfdb6651a</a></span>
             </div>
